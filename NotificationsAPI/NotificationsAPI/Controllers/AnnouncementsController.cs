d6910ca951c5a8c6fd1b9679c85fc6d29d254476
TabacaruAndreiC <116145443+TabacaruAndreiC@users.noreply.github.com>
2023-05-06 15:50:11 +0300

    Add files via upload

---
+++ NotificationsAPI/NotificationsAPI/Controllers/AnnouncementsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationsAPI.Models;
+
+namespace NotificationsAPI.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class AnnouncementsController : ControllerBase
+	{
+		static List<Announcement> _announcements = new List<Announcement> { new Announcement { Id = Guid.NewGuid(), CategoryId = "1", Title = "First Announcement", Description = "First Announcement Description" , Author = "Author_1"},
+		new Announcement { Id = Guid.NewGuid(), CategoryId = "1", Title = "Second Announcement", Description = "Second Announcement Description", Author = "Author_1" },
+		new Announcement { Id = Guid.NewGuid(), CategoryId = "1", Title = "Third Announcement", Description = "Third Announcement Description", Author = "Author_2"  },
+		new Announcement { Id = Guid.NewGuid(), CategoryId = "1", Title = "Fourth Announcement", Description = "Fourth Announcement Description", Author = "Author_3"  },
+		new Announcement { Id = Guid.NewGuid(), CategoryId = "1", Title = "Fifth Announcement", Description = "Fifth Announcement Description", Author = "Author_4"  }
+		};
+		
+		/// <summary>
+		/// Gets all the announcements from the repository
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public IActionResult GetAnnouncements()
+		{
+			return Ok(_announcements);
+		}
+
+		[HttpPost]
+		public IActionResult CreateAnnouncement([FromBody] Announcement? announcement)
+		{
+			if (announcement == null)
+			{
+				return BadRequest("Announcement cannot be null");
+			}
+			_announcements.Add(announcement);
+			return Ok(_announcements);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateAnnouncement([FromBody] Announcement? announcement)
+		{
+			if (announcement == null)
+			{
+				return BadRequest("Announcement cannot be null");
+			}
+
+			var announcementInApp = _announcements.FirstOrDefault(a => a.Id.Equals(announcement.Id));
+			if (announcementInApp == null)
+			{
+				_announcements.Add(announcement);
+			}
+			else
+			{
+				_announcements[_announcements.IndexOf(announcementInApp)] = announcement;
+			}
+			return Ok(announcement);
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteAnnouncement([FromBody] Guid id)
+		{
+			var announcement = _announcements.Find(a => a.Id.Equals(id));
+
+			if(announcement == null)
+			{
+				return NotFound(id);
+			}
+			else
+			{
+				_announcements.Remove(announcement);
+				return Ok(announcement);
+			}
+		}
+	}
+}
---
+++ NotificationsAPI/NotificationsAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationsAPI.Models;
+
+namespace NotificationsAPI.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private static List<Category> _categories = new()
+		{
+			new Category{Id = 1, Name = "General"},
+			new Category{Id = 2, Name = "Course"},
+			new Category{Id = 3, Name = "Laboratory"}
+		};
+
+		[HttpGet]
+		public IActionResult GetCategories()
+		{
+			return Ok(_categories);
+		}
+
+		[HttpPost]
+		public IActionResult AddCategory([FromBody] Category? category)
+		{
+			if (category == null)
+			{
+				return BadRequest("Category cannot be null");
+			}
+
+			_categories.Add(category);
+
+			return Ok(category);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateCategory([FromBody] Category? category)
+		{
+			if (category == null)
+			{
+				return BadRequest("Category cannot be null");
+			}
+
+			var categoryInApp = _categories.FirstOrDefault(c => c.Id == category.Id);
+
+			if(categoryInApp == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				_categories[_categories.IndexOf(categoryInApp)] = category;
+				return Ok(category);
+			}
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteCategory([FromBody] int categoryId)
+		{
+			var category = _categories.FirstOrDefault(x => x.Id == categoryId);
+			
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			_categories.Remove(category);
+
+			return Ok(category);
+		}
+
+	}
+}
